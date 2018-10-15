3d319e256b409a652ab8eb158a56aa1c5b437b8a
mrlee1299 <44149392+mrlee1299@users.noreply.github.com>
2018-10-15 15:07:20 +0900

    Update and rename README.md to README.cs

---
+++ README.cs
@@ -0,0 +1,20 @@
+# text
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string gender = Console.ReadLine();
+            Console.WriteLine($"Hello World. {name}. Your Gender is {gender}.\n");
+        }
+    }
+}
+
